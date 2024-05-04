da4ad3aa6ba34948ec0a4cb3b9261d52a1e63ac0
Sapar S <sapar26@mail.ru>
2024-05-04 14:42:00 +0300

    разбил на компоненты

---
+++ src/view/new-event-button-view.js
@@ -0,0 +1,24 @@
+import {createElement} from '../render.js';
+
+
+function createEventButtonTemplate() {
+  return `<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>`;
+}
+
+export default class NewEventButton {
+  getTemplate() {
+    return createEventButtonTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
